5e456808d1346542b498057a7c96c55217543506
Sadman Sobhan <imran110219@gmail.com>
2025-03-29 02:42:17 +0600

    social organization api

--- src/main/java/com/ciphertext/opencarebackend/controller/HospitalApiController.java
+++ src/main/java/com/ciphertext/opencarebackend/controller/HospitalApiController.java
@@ -1,5 +1,6 @@
 package com.ciphertext.opencarebackend.controller;
 
+import com.ciphertext.opencarebackend.dto.filter.HospitalFilter;
 import com.ciphertext.opencarebackend.dto.request.HospitalRequest;
 import com.ciphertext.opencarebackend.dto.response.HospitalResponse;
 import com.ciphertext.opencarebackend.entity.Hospital;
@@ -8,10 +9,16 @@ import com.ciphertext.opencarebackend.mapper.HospitalMapper;
 import com.ciphertext.opencarebackend.service.HospitalService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -22,7 +29,44 @@ public class HospitalApiController {
     private final HospitalService hospitalService;
     private final HospitalMapper hospitalMapper;
 
-    @GetMapping
+    @GetMapping("")
+    public ResponseEntity<Map<String, Object>> getAllHospitalsPage(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String bnName,
+            @RequestParam(required = false) Integer numberOfBed,
+            @RequestParam(required = false) List<Integer> districtIds,
+            @RequestParam(required = false) Integer upazillaId,
+            @RequestParam(required = false) Integer unionId,
+            @RequestParam(required = false) List<String> hospitalTypes,
+            @RequestParam(required = false) String organizationType,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size) {
+
+        Pageable pagingSort = PageRequest.of(page, size);
+        HospitalFilter hospitalFilter = HospitalFilter.builder()
+                .name(name)
+                .bnName(bnName)
+                .numberOfBed(numberOfBed)
+                .districtIds(districtIds)
+                .upazilaId(upazillaId)
+                .unionId(unionId)
+                .hospitalTypes(hospitalTypes)
+                .organizationType(organizationType)
+                .build();
+        Page<Hospital> pageHospitals = hospitalService.getPaginatedDataWithFilters(hospitalFilter, pagingSort);
+
+        Page<HospitalResponse> pageHospitalResponses = pageHospitals.map(hospitalMapper::toResponse);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("hospitals", pageHospitalResponses.getContent());
+        response.put("currentPage", pageHospitals.getNumber());
+        response.put("totalItems", pageHospitals.getTotalElements());
+        response.put("totalPages", pageHospitals.getTotalPages());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
     public ResponseEntity<List<HospitalResponse>> getAllHospitals() {
         log.info("Retrieving all hospitals");
 
--- src/main/java/com/ciphertext/opencarebackend/controller/InstitutionApiController.java
+++ src/main/java/com/ciphertext/opencarebackend/controller/InstitutionApiController.java
@@ -1,16 +1,25 @@
 package com.ciphertext.opencarebackend.controller;
 
+import com.ciphertext.opencarebackend.dto.filter.InstitutionFilter;
 import com.ciphertext.opencarebackend.dto.response.InstitutionResponse;
+import com.ciphertext.opencarebackend.dto.response.InstitutionResponse;
+import com.ciphertext.opencarebackend.entity.Institution;
 import com.ciphertext.opencarebackend.entity.Institution;
 import com.ciphertext.opencarebackend.exception.ResourceNotFoundException;
 import com.ciphertext.opencarebackend.mapper.InstitutionMapper;
 import com.ciphertext.opencarebackend.service.InstitutionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -21,7 +30,42 @@ public class InstitutionApiController {
     private final InstitutionService institutionService;
     private final InstitutionMapper institutionMapper;
 
-    @GetMapping
+    @GetMapping("")
+    public ResponseEntity<Map<String, Object>> getAllInstitutionsPage(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String bnName,
+            @RequestParam(required = false) Integer enroll,
+            @RequestParam(required = false) List<Integer> districtIds,
+            @RequestParam(required = false) String country,
+            @RequestParam(required = false) List<String> hospitalTypes,
+            @RequestParam(required = false) String organizationType,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size) {
+
+        Pageable pagingSort = PageRequest.of(page, size);
+        InstitutionFilter institutionFilter = InstitutionFilter.builder()
+                .name(name)
+                .bnName(bnName)
+                .districtIds(districtIds)
+                .enroll(enroll)
+                .organizationType(organizationType)
+                .hospitalTypes(hospitalTypes)
+                .country(country)
+                .build();
+        Page<Institution> pageInstitutions = institutionService.getPaginatedDataWithFilters(institutionFilter, pagingSort);
+
+        Page<InstitutionResponse> pageInstitutionResponses = pageInstitutions.map(institutionMapper::toResponse);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("institutions", pageInstitutionResponses.getContent());
+        response.put("currentPage", pageInstitutions.getNumber());
+        response.put("totalItems", pageInstitutions.getTotalElements());
+        response.put("totalPages", pageInstitutions.getTotalPages());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
     public ResponseEntity<List<InstitutionResponse>> getAllInstitutions() {
         log.info("Retrieving all institutions");
 
