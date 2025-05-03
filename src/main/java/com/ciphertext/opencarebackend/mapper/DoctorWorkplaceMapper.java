b65a6b2eee194e918745c09c64ec352ccd82e904
Sadman Sobhan <imran110219@gmail.com>
2025-05-03 23:59:44 +0600

    add README.md

--- src/main/java/com/ciphertext/opencarebackend/mapper/DoctorWorkplaceMapper.java
+++ src/main/java/com/ciphertext/opencarebackend/mapper/DoctorWorkplaceMapper.java
@@ -3,12 +3,14 @@ package com.ciphertext.opencarebackend.mapper;
 import com.ciphertext.opencarebackend.dto.request.DoctorWorkplaceRequest;
 import com.ciphertext.opencarebackend.dto.response.DoctorWorkplaceResponse;
 import com.ciphertext.opencarebackend.entity.DoctorWorkplace;
+import com.ciphertext.opencarebackend.enums.TeacherPosition;
 import org.mapstruct.*;
 import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 @Component
 public interface DoctorWorkplaceMapper {
+    @Mapping(target = "teacherPosition", source = "teacherPosition", qualifiedByName = "mapTeacherPosition")
     DoctorWorkplaceResponse toResponse(DoctorWorkplace doctorWorkplace);
 
     DoctorWorkplace toEntity(DoctorWorkplaceRequest request);
@@ -16,4 +18,8 @@ public interface DoctorWorkplaceMapper {
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void partialUpdate(DoctorWorkplaceRequest request, @MappingTarget DoctorWorkplace doctorWorkplaceDegree);
 
+    @Named("mapTeacherPosition")
+    default String mapTeacherPosition(TeacherPosition position) {
+        return position != null ? position.name() : null;
+    }
 }
